fc7d9af4afd496496461b5e83d4abb5de48cb41f
Daniel <dani.mbirochi@gmail.com>
2023-06-29 07:47:43 -0300

    Playing with Closures & Iterators

--- exercise/docs/src/lib.rs
+++ exercise/docs/src/lib.rs
@@ -47,7 +47,7 @@ impl Pumpkin {
 // 5. Document that BURNT_ORANGE is for the "orangeness" field in the Pumpkin struct.
 // - Link to the Pumpkin struct in your description
 
-/// BURNT_ORANGE is for the "orangeness" field in the [Pumpkin] struct.
+/// `BURNT_ORANGE` is for the "orangeness" field in the [Pumpkin] struct.
 pub const BURNT_ORANGE: i32 = 13;
 
 // Challenge: Find the option to pass to `cargo doc` so that documentation for this private item
